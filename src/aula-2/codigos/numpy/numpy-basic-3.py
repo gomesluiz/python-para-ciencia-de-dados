7a3d3f0ff7baa6213f38bef234b345ee852ffd9c
Luiz Alberto <gomes.luiz@gmail.com>
2019-05-14 01:10:18 -0300

    numpy básico

---
+++ src/aula-2/codigos/numpy/numpy-basic-3.py
@@ -0,0 +1,9 @@
+import numpy as np 
+imcs
+# array([21.85171573, 20.97505669, 21.75028214, 24.7473475, 21.44127836])
+imcs[1]
+# 21.85171573
+imcs > 23 
+# array([False, False, False, True, False])
+imcs[imcs > 23] 
+# array([24.7473475])
